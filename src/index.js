f8fb32de84c237f498d20a32df9e05e19f160b27
Duane David <duanedave17@gmail.com>
2022-08-09 06:38:54 -0700

    Update linters.yml

--- src/index.js
+++ src/index.js
@@ -2,36 +2,37 @@
 import _ from 'lodash';
 import './style.css';
 
-const todoList = [
-  {
-    id: 1,
-    text: 'Take out the trash',
-    completed: false,
-  },
-  {
-    id: 2,
-    text: 'Go to bed',
-    completed: false,
-  },
-  {
-    id: 3,
-    text: 'Take baby to school',
-    completed: false,
-  },
-];
+const todoList = [];
+const input = document.querySelector('input');
 
 const renderTodoList = () => {
   const todoContainer = document.querySelector('.todo-container');
-
-  todoList.forEach((todo) => {
-    todoContainer.innerHTML += `
+  todoContainer.innerHTML += `
     <div class="todo border-bottom flex">
       <input class="box" type="checkbox" />
-      <input type="text" value="${todo.text}" />
-      <i class="bx bx-dots-vertical-rounded"></i>
+      <input type="text" value="${input.value}" />
+      <i class='bx bx-trash'></i>
     </div>
     `;
-  });
 };
 
-renderTodoList();
+const removeTodo = (e) => {
+  const todo = e.target.parentElement;
+  todo.remove();
+};
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  todoList.push({
+    id: todoList.length + 1,
+    text: input.value,
+    completed: false,
+  });
+  renderTodoList();
+
+  input.value = '';
+});
+
+const deleteBtn = document.querySelectorAll('.bx-trash');
+deleteBtn.addEventListener('click', removeTodo);
