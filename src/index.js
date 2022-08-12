cdbb1014139fd410719f01745fd42f067d8544b9
Duane David <duanedave17@gmail.com>
2022-08-12 03:04:41 -0700

    add interractive function

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import TodoItem from './modules/TodoItem.js';
 
 const form = document.querySelector('form');
 const todoWrapper = document.querySelector('.todo-container');
+const removeCompleted = document.querySelector('button');
 
 const todoArray = new TodoArray();
 
@@ -12,16 +13,20 @@ const renderTodos = () => {
   if (todoArray.getAllTodos().length === 0) {
     todoWrapper.innerHTML = '<h3 class= "alert">Todo is Empty</h3>';
   } else {
-    todoArray.getAllTodos().forEach((todo, id) => {
+    todoArray.getAllTodos().forEach((todo, index) => {
       const todoItem = document.createElement('div');
       todoItem.classList.add('todo-item');
+      const todoStatus = () => {
+        const status = todo.completed ? 'checked' : '';
+        return status;
+      };
       todoItem.innerHTML = `
-      <div data-check = ${id} class="todo border-bottom flex">
-        <input class="box" type="checkbox" />
-        <input data-item = ${todo.id} class="todo-item" type="text" value="${todo.description}" />
-        <i id="delete-btn" data-remote = ${id} class='bx bx-trash' id="delete-btn"></i>
-      </div>
-    `;
+        <div data-check = ${index} class="todo border-bottom flex">
+        <input data-complete = ${todo.id} class="box" ${todoStatus()} type="checkbox" />
+        <input data-item = ${todo.id} class="item ${todoStatus()}" type="text" value="${todo.description}" />
+        <i id="delete-btn" data-remote = ${index} class='bx bx-trash' id="delete-btn"></i>
+        </div>
+      `;
       todoWrapper.appendChild(todoItem);
     });
   }
@@ -29,29 +34,53 @@ const renderTodos = () => {
   const deletBtn = document.querySelectorAll('#delete-btn');
   deletBtn.forEach((btn) => {
     btn.addEventListener('click', (e) => {
-      const id = e.target.dataset.remote;
-      todoArray.deleteTodo(id);
+      const { remote } = e.target.dataset;
+      todoArray.deleteTodo(remote);
       renderTodos();
     });
   });
 
   const editTodo = document.querySelectorAll('.todo-item');
+  const checkedBox = document.querySelectorAll('.box');
   editTodo.forEach((todo) => {
-    todo.addEventListener('keyup', (e) => {
-      const id = e.target.dataset.item;
-      const description = e.target.value.trim();
+    todo.addEventListener('blur', (e) => {
+      const { dataset, value } = e.target;
+      const id = dataset.item;
+      const description = value.trim();
       const completed = false;
       const newTodo = new TodoItem(description, completed, id);
       todoArray.updateTodo(id, newTodo);
+      checkedBox[id - 1].checked = false;
+      todo.classList.remove('checked');
     });
+  });
+
+  const todoItems = document.querySelectorAll('.item');
+  const chexkbox = document.querySelectorAll('.box');
+  chexkbox.forEach((checkbox) => {
+    checkbox.addEventListener('click', (e) => {
+      const { complete } = e.target.dataset;
+      if (checkbox.checked) {
+        todoArray.toggleCompleted(complete);
+        todoItems[complete - 1].classList.add('checked');
+      } else {
+        todoArray.toggleCompleted(complete);
+        todoItems[complete - 1].classList.remove('checked');
+      }
+    });
+  });
+
+  removeCompleted.addEventListener('click', () => {
+    todoArray.clearCompleted();
+    renderTodos();
   });
 };
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const todoDescription = document.querySelector('.input').value;
+  const todoText = document.querySelector('.input').value;
   const todo = new TodoItem(
-    todoDescription,
+    todoText,
     false,
     todoArray.getAllTodos().length + 1,
   );
